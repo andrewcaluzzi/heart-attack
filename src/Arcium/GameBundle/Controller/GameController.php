de93ac1af5bbb7765e1e309705ba70156c805af2
Andrew Caluzzi <andrew.caluzzi@elanmediapartners.com>
2015-01-27 00:37:49 +1100

    Added examples of GameManager function use, added reminders/TODOs for future work relating to persistent draw pile handling

--- src/Arcium/GameBundle/Controller/GameController.php
+++ src/Arcium/GameBundle/Controller/GameController.php
@@ -20,6 +20,12 @@ class GameController extends Controller
 
         /** @var $gm Services\GameManager */
         $gm = $this->get('gamemanager');
+        $cards = $gm->getCardString(array('Ah', '7c', '2c', '5c'));
+        $turn->setCards($cards);
+
+        ## Reminder: use [php app/console server:run localhost] for quick ad-hoc server
+        ## TODO - write drawCard() function in Game object
+        ## TODO - maintain draw pile, as a string? needs to be saved in Game object
 
         return $this->render('ArciumGameBundle:Default:index.html.twig',
             array(
